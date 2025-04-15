f8ba307a76333bf29f9895c3c801e520cd9e31fb
Luis Andrés Santiago Torres <luis.santiagot@vasscompany.com>
2025-04-15 19:48:08 +0200

    vite.config change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 // Exportamos la configuración de Vite que recibe un objeto
 export default defineConfig({
 
-    base: '/blackjack-vite/', // Ruta base para el despliegue en GitHub Pages
+    base: '/js-vite-blackjack/', // Ruta base para el despliegue en GitHub Pages
 
 })
